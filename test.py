86d396c954f7951074898bf54ad1e99c9c720163
the brogrammer <orosand@putneyschool.org>
2018-01-12 21:13:04 -0500

    figure out how to make the arrays all go into the multidimensional array, for some readon now all of the slots in the array must be arrays,

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from PIL import Image
-
+import numpy
 imag = Image.open('0.jpg').convert('L')  # convert image to 8-bit grayscale
+imag2 = Image.open('img_1.jpg').convert('L')  # convert image to 8-bit grayscale
 	
-
-def convertImg(img):
+picArray = numpy.empty([2,100], dtype = object)
+def convertImg(img,numb,result):
 	WIDTH, HEIGHT = img.size
-	picArray = 
+	value = [] 
 
 	data = list(img.getdata()) # convert image data to a list of integers
 	# convert that to 2D list (list of lists of integers)
@@ -13,12 +14,15 @@ def convertImg(img):
 
 	# At this point the image's pixels are all in memory and can be accessed
 	# individually using data[row][col].
-
-	# For example:
 	for x in range(0,27):
 		for y in range(0,27): 
-			print data[x][y]
-
+			value.append(data[x][y])
+	picArray[numb][0] = value
+	picArray[numb][1] = result
+	print picArray
 	# Here's another more compact representation.
 
-convertImg(imag)
+convertImg(imag,0,0)
+convertImg(imag2,1,2)
+
+
