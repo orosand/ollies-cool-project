946a3d06a2b136de413477a66c3ad33ded5b586d
the brogrammer <orosand@putneyschool.org>
2018-01-22 11:49:01 -0500

    used test data

---
+++ trainingSet/network.py
@@ -0,0 +1,42 @@
+# array 784x10
+# array for each pixel
+# array 10x1
+# assign random values to each
+# save weights to a csv
+# perturb weight, randomize within a min and max with a prob and an amount it can be changed
+
+
+import numpy
+import os
+import random
+import csv
+#make the weights array
+weights = numpy.zeros([784,10], dtype = int)
+for x in range(784):
+	for y in range(10):
+		weights[x][y] = random.randint(0,10)
+pixels = [] 
+
+#open the array and convert it into the pixel array
+def openConvert(filename):
+	with open('array.csv') as csvfile:
+		data = list(csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)) # change contents to floats
+		print(data)
+		# for row in reader:
+			
+		# 	data = numpy.asarray(row)
+		# 	pixels = data
+		# 	break
+
+
+def perturb(weightX,weightY, chance, min, max):
+	percent = random.randint(0,100)
+	if (percent<=chance):
+		weights[weightX][weightY] = weights[weightX][weightY] + random.randint(min,max)
+
+perturb(12,2,18,-1,1)
+# print weights[12][2]
+
+openConvert('')
+
+
